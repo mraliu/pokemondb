c92894fb29975ecd32103fe563060a152c0a2716
mraliu <teachmrliu@gmail.com>
2024-05-16 22:55:58 +0100

    Treeview that updates

---
+++ treeviewchangeview.py
@@ -0,0 +1,71 @@
+import tkinter as tk, sqlite3
+from tkinter import ttk
+conn = sqlite3.connect("pokemon.db")
+
+def getfieldheadings():
+    cur = conn.cursor()
+    sql = 'SELECT * FROM pokemon'
+    headings = cur.execute(sql)
+    return headings
+
+def searchdb(ptype):
+    cur = conn.cursor()
+    sql = f'SELECT * FROM pokemon WHERE "Type 1" = "{ptype}"'
+    res = cur.execute(sql)
+    # Clear the treeview
+    for item in treeview.get_children():
+      treeview.delete(item)
+    # Loop through db and add to treeview
+    for record in res.fetchall():
+        treeview.insert("", tk.END, text=record[0], values=record[1:])
+
+headings = getfieldheadings()
+heading = []
+
+for field in headings.description:
+    heading.append(field[0])
+
+heading = tuple(heading)
+
+root = tk.Tk()
+root.title("DB window")
+root.geometry("800x600")
+
+scrollbarv = tk.Scrollbar(root, orient='vertical')
+scrollbarh = tk.Scrollbar(root, orient='horizontal')
+
+grassbtn = tk.Button(root, text="Grass", command=lambda x="Grass":searchdb(x), bg="Green")
+grassbtn.place(x = 50, y = 75, width = 100, height = 20)
+
+firebtn = tk.Button(root, text="Fire", command=lambda x="Fire":searchdb(x), bg="Red")
+firebtn.place(x = 150, y = 75, width = 100, height = 20)
+
+waterbtn = tk.Button(root, text="Water", command=lambda x="Water":searchdb(x), bg="Aqua")
+waterbtn.place(x = 250, y = 75, width = 100, height = 20)
+
+bugbtn = tk.Button(root, text="Bug", command=lambda x="Bug":searchdb(x), bg="Brown")
+bugbtn.place(x = 350, y = 75, width = 100, height = 20)
+
+treeview = ttk.Treeview(columns=heading[1:])
+
+for idx, head in enumerate(heading):
+    if idx==0:
+        treeview.column("#0", anchor=tk.CENTER, stretch=tk.NO, width=100)
+        treeview.heading("#0", text=head)
+    else:
+        treeview.column(head, anchor=tk.CENTER, stretch=tk.NO, width=100)
+        treeview.heading(head, text=head)
+
+treeview.place(x=50,y=125, width=700, height=150)
+
+scrollbarv.place(x=759, y=125, height=150) 
+scrollbarh.place(x=50, y=275, width=700)
+
+scrollbarv.config(command = treeview.yview)
+scrollbarh.config(command = treeview.xview)
+
+
+
+
+
+root.mainloop()
--- win.py
+++ win.py
@@ -2,10 +2,13 @@ import tkinter as tk, sqlite3
 from tkinter import ttk
 conn = sqlite3.connect("pokemon.db")
 
-cur = conn.cursor()
-sql = "SELECT * FROM pokemon"
-res = cur.execute(sql)
+def searchdb():
+    cur = conn.cursor()
+    sql = "SELECT * FROM pokemon"
+    res = cur.execute(sql)
+    return res
 
+res = searchdb()
 heading = []
 
 for field in res.description:
@@ -20,6 +23,15 @@ root.geometry("800x600")
 scrollbarv = tk.Scrollbar(root, orient='vertical')
 scrollbarh = tk.Scrollbar(root, orient='horizontal')
 
+searchlbl = tk.Label(root, text="Search ")
+searchlbl.place(x = 50, y = 75)
+
+searchentry = tk.Entry(root)
+searchentry.place(x = 100, y = 75)
+
+searchbtn = tk.Button(root, text="Enter")
+searchbtn.place(x = 230, y = 75,width = 80, height = 20)
+
 treeview = ttk.Treeview(columns=heading[1:])
 
 for idx, head in enumerate(heading):
@@ -30,13 +42,13 @@ for idx, head in enumerate(heading):
         treeview.column(head, anchor=tk.CENTER, stretch=tk.NO, width=100)
         treeview.heading(head, text=head)
 
+for record in res.fetchall():
+    treeview.insert("", tk.END, text=record[0], values=record[1:])
 
-# # treeview.insert("", tk.END, text="#", values=('Name', 'Type 1', 'Type 2', 'Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary'))
+treeview.place(x=50,y=125, width=700, height=300)
 
-treeview.place(x=50,y=125, width=700)
-
-scrollbarv.place(x=759, y=125, height=250) 
-scrollbarh.place(x=50, y=375, width=700)
+scrollbarv.place(x=759, y=125, height=300) 
+scrollbarh.place(x=50, y=425, width=700)
 
 scrollbarv.config(command = treeview.yview)
 scrollbarh.config(command = treeview.xview)
